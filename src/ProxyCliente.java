bdc7d4c6a6da90c107d8135f13f2b55742593e2f
RomarioLopCa <romario_lopezc@hotmail.com>
2014-10-14 23:20:10 -0500

    prueba1.0

--- src/ProxyCliente.java
+++ src/ProxyCliente.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -17,59 +16,55 @@ import java.util.ArrayList;
  * @author Romario
  */
 public class ProxyCliente {
-    public int[] numeros;
+
     ArrayList<Candidatos> candidatos;
     public String datosServicio;
-    
-    public void Connect(String clienteDice,ArrayList candidatos){
-        this.candidatos=candidatos;
-        String hostNameBroker = "192.168.230.149";
-        //int portNumber = (int)this.ListaDeBrokers.get(broker);
+
+    public void Connect(String clienteDice, ArrayList candidatos) {
+        this.candidatos = candidatos;
+        String hostNameBroker = "127.0.0.1";
         int portNumberBroker = 4444;
         try (
-            Socket kkSocket = new Socket(hostNameBroker, portNumberBroker);
-            PrintWriter aBroker = new PrintWriter(kkSocket.getOutputStream(), true);
-            BufferedReader deBroker = new BufferedReader(
-                new InputStreamReader(kkSocket.getInputStream()));
-        ) {
-            BufferedReader stdIn =
-                new BufferedReader(new InputStreamReader(System.in));
+                Socket kkSocket = new Socket(hostNameBroker, portNumberBroker);
+                PrintWriter aBroker = new PrintWriter(kkSocket.getOutputStream(), true);
+                BufferedReader deBroker = new BufferedReader(
+                        new InputStreamReader(kkSocket.getInputStream()));) {
+            BufferedReader stdIn
+                    = new BufferedReader(new InputStreamReader(System.in));
             String fromBroker;
             String fromClient;
-            
+
             while ((fromBroker = deBroker.readLine()) != null) {
                 System.out.println("Broker: " + fromBroker);
-                if (fromBroker.equals("Bye."))
+                if (fromBroker.equals("Bye.")) {
                     break;
-                
-                fromClient = clienteDice;
-                if (fromClient != null) {
-                    if(fromClient.toLowerCase().contains("servicio")){
-                        packData(numeros);
-                        fromClient +=","+datosServicio;
-                        aBroker.println(fromClient);
-                    }else
-                    aBroker.println(fromClient);
                 }
+
+                fromClient = clienteDice;
+                packData();
+                fromClient += "," + datosServicio;
+                aBroker.println(fromClient);
+
             }
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostNameBroker);
             System.exit(1);
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                hostNameBroker);
+            System.err.println("Couldn't get I/O for the connection to "
+                    + hostNameBroker);
             System.exit(1);
         }
-    
+
     }
-    
-    public void packData(int[] numeros){
-        datosServicio ="";
-        
-        for (int num : numeros) {
-            datosServicio+= num+"|";
+
+    public void packData() {
+        datosServicio = "";
+
+        for (Candidatos cand : candidatos) {
+            String dato = cand.getNombre() + "|" + cand.getVotos() + "%";
+            datosServicio += dato;
         }
-        
+
         System.out.println(datosServicio);
     }
 }
